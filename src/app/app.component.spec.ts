7bf7f061bdae277b49ef6a5f6360461cf7f44fc9
Udo Klarhöfer <klarhoefer@gmail.com>
2024-08-18 16:52:13 +0200

    Add mocks to tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+// import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
+        // HttpClientTestingModule
         HttpClientModule
       ],
       declarations: [
@@ -25,5 +27,14 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Test Dummy');
+  });
+
+  it('should have two clients', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const component = fixture.componentInstance;
+    component.ngOnInit();
+    // expect(compiled.querySelectorAll('app-client').length).toBe(2);
   });
 });
--- src/app/clients.service.spec.ts
+++ src/app/clients.service.spec.ts
@@ -1,21 +1,38 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ClientsService } from './clients.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('ClientsService', () => {
   let service: ClientsService;
+  let httpCtrl: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientModule
+        HttpClientTestingModule
       ]
     });
+
     service = TestBed.inject(ClientsService);
+    httpCtrl = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should receive custoers', () => {
+    service.loadClients().subscribe(clients => {
+      expect(clients.length).toBe(2);
+      expect(clients[0].id).toBe(11);
+    });
+
+    const req = httpCtrl.expectOne('http://localhost:3000/customers');
+
+    req.flush([
+      { id: 11, name: 'Horst' },
+      { id: 12, name: 'Wolfgang' }
+    ]);
   });
 });
